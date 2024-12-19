23e2dbecfa6fb3e7ea91996cbb50600c931d4b5c
Rene314159 <rq3.141592@gmail.com>
2024-12-18 20:15:44 -0500

    added docker postgres 15 image as db, it is running

--- doc_collab/settings.py
+++ doc_collab/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # local apps
+
+    'doc_collab_app.apps.DocCollabAppConfig',
+
+    # 3rd-party apps
+
+    'channels',
+
 ]
 
 MIDDLEWARE = [
@@ -73,13 +82,17 @@ WSGI_APPLICATION = 'doc_collab.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'doc_collab_db',
+        'USER': 'doc_collab_db_user',
+        'PASSWORD': 'doc_collab_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
